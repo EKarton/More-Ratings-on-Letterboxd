8457887ed6a93e13eac3f8c6381412bf3ad1cc27
Emilio Kartono <emiliokartono@gmail.com>
2021-08-31 02:54:00 -0400

    fix: fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const contentScripts = {
